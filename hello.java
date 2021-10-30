9e1d3fd40183597a8962fed0f82c6bd734c7df5b
admiralwaffle4 <56054320+admiralwaffle4@users.noreply.github.com>
2021-10-29 20:17:34 -0400

    Added the whole FTC thing to the folder

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class hello {
+    static void plastic() {
+        System.out.println("PLASTIC HEARTS");
+    }
+    public static void main(String[] args) {
+        System.out.println("AAAAAAAAAAAAA");
+        plastic();
+    }
+}
