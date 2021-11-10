43bb010269a77fd45208e50e27d3e6cafca38782
admiralwaffle4 <56054320+admiralwaffle4@users.noreply.github.com>
2021-11-10 15:15:09 -0500

    Update Hardware.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -22,87 +22,35 @@ public class vitiHardware {
 
 
     public void init(HardwareMap hardwareMap) {
-        frontleft = hardwareMap.get(DcMotor.class,  "frontleft");
-        frontright = hardwareMap.get (DcMotor.class, "frontright");
-        backleft = hardwareMap.get (DcMotor.class, "backright");
-        backright = hardwareMap.get (DcMotor.class, "backleft");
+        left = hardwareMap.get(DcMotor.class,  "left");
+        right = hardwareMap.get (DcMotor.class, "right");
 
-        frontright.setDirection(DcMotor.Direction.FORWARD);
-        frontleft.setDirection(DcMotor.Direction.REVERSE);
-        backleft.setDirection(DcMotor.Direction.REVERSE);
-        backright.setDirection(DcMotor.Direction.FORWARD);
+        right.setDirection(DcMotor.Direction.FORWARD);
+        left.setDirection(DcMotor.Direction.REVERSE);
 
-        frontleft.setPower(0);
-        frontright.setPower(0);
-        backright.setPower(0);
-        backleft.setPower(0);
-
-        intakeright = hardwareMap.get(DcMotor.class, "intakeright");
-        intakeleft = hardwareMap.get(DcMotor.class, "intakeleft");
-
-        intakeright.setDirection(DcMotor.Direction.FORWARD);
-        intakeleft.setDirection(DcMotor.Direction.REVERSE);
-
-        intakeright.setPower(0);
-        intakeleft.setPower(0);
-
-        lift = hardwareMap.get (DcMotor.class, "lift");
-
-        lift.setPower(0);
-
-        claw = hardwareMap.get(Servo.class, "claw");
-
-        claw.setDirection(Servo.Direction.FORWARD);
-
-        claw.setPosition(-1);
-
-        flipper = hardwareMap.get (Servo.class, "flipper");
-
-        flipper.setPosition(-1);
-
-        swivel = hardwareMap.get(Servo.class, "swivel");
-
-        swivel.setDirection(Servo.Direction.FORWARD);
-
-        swivel.setPosition(-1);
-
-        spool = hardwareMap.get (DcMotor.class, "spool");
-
-        spool.setDirection(DcMotor.Direction.FORWARD);
-
-        spool.setPower(-1);
-
-
-
+        left.setPower(0);
+        right.setPower(0);
 
     }
     // Sets motors to run without encoders
     public void noEncoders() {
-        frontleft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        frontright.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        backleft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        backright.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fronright.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
     public void encoders() {
-        frontleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     public void resetEncoders() {
-        frontleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        frontright.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backright.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
     public void toPosition() {
-        frontleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        frontright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 }
 
